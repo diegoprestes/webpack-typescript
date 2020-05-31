ec75dc31b442d081221df010d0e3d345e16cc479
Diego Prestes <diego.prestes@possible.com>
2020-05-31 09:21:31 -0300

    added static folder to store static files

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.ts',
@@ -18,5 +19,12 @@ module.exports = {
     publicPath: 'dist',
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: path.resolve(__dirname, 'static'), to: path.resolve(__dirname, 'dist') },
+      ],
+    }),
+  ],
 };
